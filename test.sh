5ec11ad0bf994774ee44c96270c5e41a0a75d343
Kotaro Noguchi <kotaro.noguchi@linecorp.com>
2025-11-08 10:21:09 +0900

    Verify multiple scenario (copied shell script from https://github.com/rui314/chibicc/commit/f722daaaae0606115df4ace5a852da23c1a5b0f3#diff-3722d9ba8feb2d3feac8ce71a209a638d4b404e1c53f937188761181594023e2)

--- test.sh
+++ test.sh
@@ -1,4 +1,22 @@
-cargo run -- 42 > tmp.s
-cc -o tmp tmp.s
-./tmp
-echo $?
+#!/bin/bash
+assert() {
+  expected="$1"
+  input="$2"
+
+  cargo run -- "$input" > tmp.s
+  cc -o tmp tmp.s
+  ./tmp
+  actual="$?"
+
+  if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+  else
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+assert 0 0
+assert 42 42
+
+echo OK
